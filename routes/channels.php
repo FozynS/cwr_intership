9a66633f2dde4b7704dd77a3c336172734d1c978
Danylo Shvydkyi <wolf44488@gmail.com>
2024-11-19 10:37:43 +0100

    update: new branch add class Broadcast

--- routes/channels.php
+++ routes/channels.php
@@ -10,6 +10,9 @@
 | used to check if an authenticated user can listen to the channel.
 |
 */
+
+use Illuminate\Support\Facades\Broadcast;
+
 Broadcast::channel('patient.{patientId}', function ($user, $patientId) {
   return (int) $user->id === (int) $patientId;
 });
